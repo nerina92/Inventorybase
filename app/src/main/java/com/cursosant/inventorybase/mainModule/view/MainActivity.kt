af95a268929668a38563f64d3d6255ae4e3931d5
Neri <nerina.uliana@maxirest.com>
2024-12-23 16:43:30 -0300

    PROYECTO BASE DESCARGADO

---
+++ app/src/main/java/com/cursosant/inventorybase/mainModule/view/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.cursosant.inventorybase.mainModule.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.cursosant.inventorybase.R
+import com.cursosant.inventorybase.addModule.view.AddProductFragment
+import com.cursosant.inventorybase.databinding.ActivityMainBinding
+import com.cursosant.inventorybase.entities.Product
+import com.cursosant.inventorybase.mainModule.view.adapters.OnClickListener
+import com.cursosant.inventorybase.mainModule.view.adapters.ProductAdapter
+import com.cursosant.inventorybase.mainModule.viewModel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
+
+/****
+ * Project: Inventory base
+ * From: com.cursosant.inventorybase.mainModule.view
+ * Created by Alain Nicolás Tello on 23/05/23 at 17:22
+ * All rights reserved 2023.
+ *
+ * All my Udemy Courses:
+ * https://www.udemy.com/user/alain-nicolas-tello/
+ * And Frogames formación:
+ * https://cursos.frogamesformacion.com/pages/instructor-alain-nicolas
+ *
+ * Coupons on my Website:
+ * www.alainnicolastello.com
+ ***/
+class MainActivity : AppCompatActivity(), OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ProductAdapter
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAdapter()
+        setupRecyclerView()
+        setupFab()
+
+        setupViewModel()
+    }
+
+    private fun setupAdapter(){
+        adapter = ProductAdapter(this)
+    }
+
+    private fun setupRecyclerView(){
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = GridLayoutManager(this@MainActivity,
+                resources.getInteger(R.integer.main_columns))
+            adapter = this@MainActivity.adapter
+        }
+    }
+
+    private fun setupFab(){
+        binding.fab.setOnClickListener { //Open add fragment
+            AddProductFragment().show(supportFragmentManager, getString(R.string.add_title))
+        }
+    }
+
+    private fun setupViewModel(){
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.getProducts().observe(this, { products ->
+            setListToAdapter(products)
+        })
+        viewModel.isWelcome().observe(this, { isWelcome ->
+            if (isWelcome){
+                Snackbar.make(binding.root, getString(R.string.main_msg_welcome), Snackbar.LENGTH_SHORT).show()
+                viewModel.setWelcome(false)
+            }
+        })
+    }
+
+    private fun setListToAdapter(products: List<Product>) {
+        adapter.submitList(products)
+    }
+
+    /**
+     * OnClickListener
+     * */
+    override fun onClick(product: Product) {
+        viewModel.setWelcome(true)
+    }
+
+    override fun onLongClick(product: Product) {
+        viewModel.deleteProduct(product)
+    }
+}
